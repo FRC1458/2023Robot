ea685c6f4aca4794ae80412d44aba0cbf044154b
Victor Gusev <gusev0219@gmail.com>
2023-02-04 12:38:03 -0800

    Limelight file added

---
+++ src/main/java/frc/robot/Limelight.java
@@ -0,0 +1,32 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+//import com.kauailabs.navx.frc.AHRS;
+
+
+
+public class Limelight {
+    NetworkTable table;
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    
+    public Limelight(){
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public void readPeriodic() {
+        //post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
+}
