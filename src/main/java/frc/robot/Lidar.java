e41139ab46c51cffded6df2eb821662b5b2f3b7a
Victor Gusev <gusev0219@gmail.com>
2023-02-04 14:55:23 -0800

    added lidar to robot constants

--- src/main/java/frc/robot/Lidar.java
+++ src/main/java/frc/robot/Lidar.java
@@ -1,17 +1,13 @@
 package frc.robot;
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.DigitalInput;
-
-import com.cuforge.libcu.Lasershark;
 
 public class Lidar {
     DutyCycle lasershark;
 
 
     public Lidar() {
-        lasershark = new DutyCycle(new DigitalInput(0));//change to robot constants
-
+        lasershark = new DutyCycle(new DigitalInput(RobotConstants.lidarPort));
     }
 
     public double getDistanceCentimeters() {
--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -81,4 +81,6 @@ public class RobotConstants {
     public final static Rotation2d frontRightAngle = new Rotation2d(0);
     public final static Rotation2d backLeftAngle = new Rotation2d(0);
     public final static Rotation2d backRightAngle = new Rotation2d(0);
+
+    public final static int lidarPort = 0;
 }
