9552c024b45e7604f8eee80f99581808f4b6974d
tavenharris <95725168+tavenharris@users.noreply.github.com>
2023-02-04 16:38:58 -0800

    Cleaned up the code. Removed excessive comments. Added support for miniNavX

--- src/main/java/frc/robot/Lidar.java
+++ src/main/java/frc/robot/Lidar.java
@@ -1,12 +1,14 @@
 package frc.robot;
-import edu.wpi.first.wpilibj.DutyCycle;
+
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 
 public class Lidar {
     DutyCycle lasershark;
 
 
     public Lidar() {
+
         lasershark = new DutyCycle(new DigitalInput(RobotConstants.lidarPort));
     }
 
