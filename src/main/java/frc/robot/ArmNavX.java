1fb8ae006f68a1571e3b780aea9eea7987fda08b
Jeremy Phan <jeremyphan50@gmail.com>
2023-02-14 17:17:49 -0800

    Update ArmNavX.java

--- src/main/java/frc/robot/ArmNavX.java
+++ src/main/java/frc/robot/ArmNavX.java
@@ -2,20 +2,31 @@ package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.I2C;
-public class ArmNavX{
-  AHRS ahrs;
-  boolean error;
 
-  public ArmNavX() {
+import java.util.ArrayList;
+
+// WARNING: UNTESTED CODE
+public class ArmNavX{
+
+  AHRS ahrs;
+  Double sampling = 4.0;
+  ArrayList<Float> buffer;
+
+  public ArmNavX(Double sampling) {
     ahrs = new AHRS(I2C.Port.kOnboard);
-  }
-  public void autonomous() {
+    this.sampling = sampling;
   }
 
   public double getPitch(){
-    return ahrs.getPitch();
+    double ret = 0;
+    buffer.add(ahrs.getPitch());
+    if (buffer.size() > sampling) {buffer.remove(0);}
+    for (double i : buffer) {ret += i;}
+    ret /= sampling;
+    return ret;
   }
 
-  public void test() {
-  }
+  //public void autonomous() {}
+  //public void test() {}
+
 }
